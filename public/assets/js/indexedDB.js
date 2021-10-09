f52f222f354379a5ff4f56cfef938c12744b4c33
ZahraMertens <82029020+ZahraMertens@users.noreply.github.com>
2021-10-09 18:24:05 +1100

    Update onupgradeneeded function in indexedDB

--- public/assets/js/indexedDB.js
+++ public/assets/js/indexedDB.js
@@ -7,3 +7,22 @@ request.onsuccess = (event) => {
     console.log(request.result)
 }
 
+//Create object store
+//Will be exected when we change the version of the db
+request.onupgradeneeded = ({ target }) => {
+    //Gives access to db object
+    const db = target.result;
+    //access interface idbdatabase and Create new object store
+    const newStore = db.createObjectStore("budgetStore"); //Collection
+    console.log(newStore);
+
+    //Create index from object store (column) to be able to query over database
+    // "transaction1" is index and "transaction2" (index itself) key path (name for column)
+    newStore.createIndex("transaction1", "transaction2");
+
+
+
+
+
+};
+
